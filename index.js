58d537d3be80a6f3819d29476b2edc66f298609f
Simona Stevkovska <s.stevkovska@linkplus-it.com>
2020-05-27 22:41:40 +0200

    add comparision query

--- index.js
+++ index.js
@@ -30,8 +30,18 @@ async function createCourse() {
 createCourse();
 
 async function getCourses() {
+    //eq (equal)
+    //ne (not equal)
+    //gt (greater then)
+    //gte (greater then or equal to)
+    //lt (less then)
+    //lte (less then or equal to)
+    //in
+    //nin (not in)
+
     const courses = await Course
-        .find({ author: 'Mosh', isPublished: 'true'})
+        //.find({ author: 'Mosh', isPublished: 'true'})
+        .find({ price: { $gt: 10, $lte: 20} })
         .limit(2)
         .sort({name: 1})          //ASC = 1, DESC = -1 
         .select({ name: 1, tags: 1 });  
