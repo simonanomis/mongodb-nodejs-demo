d72d8482f8aa83101a6e4bfbd28399ec0282c68e
Simona Stevkovska <s.stevkovska@linkplus-it.com>
2020-05-27 22:05:47 +0200

    add schema to DB

--- index.js
+++ index.js
@@ -1,5 +1,15 @@
 const mongoose = require('mongoose');
 
+//Connect to DB
 mongoose.connect('mongodb://localhost/playground')
     .then(() => console.log('Connected to MongoDB...'))
     .catch(error => console.error('Could not connect to MongoDB', error));
+
+//Create schema
+const courseSchema = new mongoose.Schema({
+    name: String,
+    author: String,
+    tags: [String],
+    date: {type: Date, default: Date.now},
+    isPublished: Boolean
+});
