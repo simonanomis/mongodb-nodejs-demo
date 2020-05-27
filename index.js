aabf2ec0bbe1a4ad55c3066f427b930c21d8e5c9
Simona Stevkovska <s.stevkovska@linkplus-it.com>
2020-05-27 22:32:28 +0200

    add getCourses query

--- index.js
+++ index.js
@@ -15,9 +15,28 @@ const courseSchema = new mongoose.Schema({
 });
 
 const Course = mongoose.model('Course', courseSchema); //class
-const course = new Course({
-    name: 'Node.js Course',
-    author: 'Simona',
-    tags: ['node', 'backend'],
-    isPublished: true
-}); //object, where there is many to many relationiship between courses and tags
+
+async function createCourse() {
+    const course = new Course({
+        name: 'Node.js Course',
+        author: 'Simona',
+        tags: ['node', 'backend'],
+        isPublished: true
+    }); //object, where there is many to many relationiship between courses and tags
+    
+    const result = await course.save(); //returns a promise
+}
+
+createCourse();
+
+async function getCourses() {
+    const courses = await Course
+        .find({ author: 'Mosh', isPublished: 'true'})
+        .limit(2)
+        .sort({name: 1})          //ASC = 1, DESC = -1 
+        .select({ name: 1, tags: 1 });  
+         //which properties we want to return from query
+    console.log(courses);
+}
+
+getCourses();
